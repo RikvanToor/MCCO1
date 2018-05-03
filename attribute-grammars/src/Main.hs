010d56ee57a6b4ba1708574ff5d7e697426eeecf
martijn <martijn@mafl.me>
2018-05-03 12:39:03 +0200

    opgave B af, telt let expressions goed

--- attribute-grammars/src/Main.hs
+++ attribute-grammars/src/Main.hs
@@ -7,7 +7,7 @@ import Data.List(nub, elemIndex, isSuffixOf, isPrefixOf, intercalate)
 import Data.Maybe(fromJust)
 import System.Directory(doesFileExist, getModificationTime,
                         getPermissions, Permissions(writable))
-import System.Exit                       
+import System.Exit
 import Utils.Messages
 import Utils.Utils
 import Main.CompileUtils
@@ -21,7 +21,7 @@ import Parser.ParseMessage
 main :: IO ()
 main = do
     args                     <- getArgs
-    (options, Just fullName) <- processArgs args   
+    (options, Just fullName) <- processArgs args
     compile fullName
     return ()
 
@@ -38,48 +38,49 @@ compile fullName =
         contents <- readSourceFile fullName
 
         -- Phase 1: Lexing
-        (lexerWarnings, tokens) <- 
+        (lexerWarnings, tokens) <-
             doPhaseWithExit 20 (const "L") compileOptions $
                phaseLexer fullName contents options
-        
+
         unless (NoWarnings `elem` options) $
             showMessages lexerWarnings
 
         putStrLn ("Parsing " ++ fullName)
 
         -- Phase 2: Parsing
-        parsedModule <- 
+        parsedModule <-
             doPhaseWithExit 20 (const "P") compileOptions $
                phaseParser fullName tokens options
 
         putStrLn ("Resolving " ++ fullName)
-               
+
         -- Phase 3: Resolving operators
-        resolvedModule <- 
+        resolvedModule <-
             doPhaseWithExit 20 (const "R") compileOptions $
                phaseResolveOperators parsedModule importEnvs options
-        
+
         putStrLn ("Checking " ++ fullName)
-        
+
         -- NOTE: the next call is when everything you do should happen.
-        -- The code that should be executed you should put in 
+        -- The code that should be executed you should put in
         -- StaticAnalysis/
         -- Phase 4: Static checking
-        nrOfLeaves <-
+        (nrOfLeaves, letDepth) <-
             doPhaseWithExit 20 (const "S") compileOptions $
                phaseStaticChecks fullName resolvedModule importEnvs options
-               
-               
-        putStrLn "Report:"       
+
+
+        putStrLn "Report:"
         putStrLn ("* Number of ast leaves: " ++ show nrOfLeaves)
-        
+        putStrLn ("* Maximum let depth: "    ++ show letDepth)
+
         putStrLn "....any other stuff you will be computing..."
-        
+
         putStrLn "Done now"
-        
+
         --unless (NoWarnings `elem` options) $
         --    showMessages staticWarnings
-           
+
 stopCompilingIf :: Bool -> IO ()
 stopCompilingIf bool = when bool (exitWith (ExitFailure 1))
 
--- attribute-grammars/src/Main/PhaseStaticChecks.hs
+++ attribute-grammars/src/Main/PhaseStaticChecks.hs
@@ -10,21 +10,21 @@ import Types (TpScheme)
 -- The ouput of this function tells you what the synthesized/chained
 -- attributes we are in fact interested in. At this point, that is simply
 -- an integer, counting the number of nodes in the ast.
-phaseStaticChecks :: 
-   String -> Module -> [ImportEnvironment] -> [Option] -> 
-   Phase Error Int
+phaseStaticChecks ::
+   String -> Module -> [ImportEnvironment] -> [Option] ->
+   Phase Error (Int, Int)
 phaseStaticChecks fullName module_ importEnvs options = do
     enterNewPhase "Static checking" options
 
     let (_, baseName, _) = splitFilePath fullName
 
-        -- Setting up two inherited attributes (to Module) 
+        -- Setting up two inherited attributes (to Module)
         -- by a suitable magic incantation
         res = SC.wrap_Module (SC.sem_Module module_) SC.Inh_Module {
             SC.importEnvironments_Inh_Module = importEnvs,
-            SC.baseName_Inh_Module = baseName 
+            SC.baseName_Inh_Module = baseName
         }
 
     -- At this point, we just return the size. Not checking for errors yet,
     -- so we are always right.
-    return (Right (SC.nrOfLeaves_Syn_Module res))
+    return (Right (SC.nrOfLeaves_Syn_Module res, SC.letDepth_Syn_Module res))
