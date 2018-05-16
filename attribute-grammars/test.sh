ac81de882f9ba22bc63a1112cd69ffead676b7a6
martijn <martijn@mafl.me>
2018-05-16 12:18:04 +0200

    Auto test met hardcoded 3 regels bovenaan elke test file die beschrijven wat er moet gebeuren in die test file.

--- attribute-grammars/test.sh
+++ attribute-grammars/test.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/bash
 
+echo -n "compiling... "
 sh ./compile.sh 1>&2 >> /dev/null
+echo "done."
 
 for testFile in $(ls examples/*.test); do
-  echo "running for test: $testFile"
+  echo ""
+  echo "--------------------------------------------------------------------------------"
+  echo "                         running for test: $testFile"
+  echo "--------------------------------------------------------------------------------"
+  echo ""
   cabal run $testFile
+
+  echo ""
+  echo "Notes from $testFile header:"
+  cat $testFile | head -n 3
 done
