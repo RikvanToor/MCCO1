089b735e5ced486818dac98f3ff175935fe6b4bb
martijn <martijn@mafl.me>
2018-05-19 14:05:03 +0200

    Ik heb de labeling gedaan en twee functies in Main toegevoegd voor het parsen van testbestanden. Ik heb nog niets getest, want er schijnt geen Show instantie te bestaan voor de datatypes uit AttributeGrammar.ag.

--- monotone-frameworks/src/Main.hs
+++ monotone-frameworks/src/Main.hs
@@ -2,6 +2,17 @@ module Main where
 
 import Lexer
 import Parser
+import AttributeGrammar
+
+-- Helper functies voor parsen van files en strings
+parse :: String -> Program
+parse = happy . alex
+
+parseFile :: FilePath -> IO Program
+parseFile f =
+  do
+    c <- readFile f
+    return (parse c)
 
 main :: IO ()
 main = undefined
