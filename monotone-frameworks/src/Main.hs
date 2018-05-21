8e1cc44fc063b296010cfad0de078a32bac086e8
martijn <martijn@mafl.me>
2018-05-21 12:12:06 +0200

    Ik heb de label functie getest en die werkt voor simpele programmas. Zodra we procedures hebben vertoont de functie wat raar gedrag (probeer maar eens te draaien op examples/fib.c). Hier moeten we nog even goed naar kijken.
    Verder heb ik wat bestanden van de tracker verwijderd die niet
    interessant zijn omdat ze automatisch gegenereerd worden (zoals
    Lexer.hs, AttributeGrammar.hs enz. enz.)

--- monotone-frameworks/src/Main.hs
+++ monotone-frameworks/src/Main.hs
@@ -4,15 +4,44 @@ import Lexer
 import Parser
 import AttributeGrammar
 
+import System.Directory
+import System.IO
+
 -- Helper functies voor parsen van files en strings
 parse :: String -> Program
 parse = happy . alex
 
 parseFile :: FilePath -> IO Program
-parseFile f =
-  do
-    c <- readFile f
-    return (parse c)
+parseFile f = parse <$> readFile f
+
+--------------------------------------------------------------------------------
+--                           1. Semantische functies                          --
+--------------------------------------------------------------------------------
+
+{- Deze functie voegt labels toe aan de AST. sem_Program is een automatisch
+ - gegenereerde functie van type Program -> T_Program, waar T_Program een type
+ - is van Int -> (Int, Program'). Dit kan dus nog veranderen wanneer we meer toe
+ - gaan voegen aan de semantiek van Program.
+ -
+ - De Int is de initiele waarde voor de programmapunten.
+ -}
+labeler :: Program -> Program'
+labeler p =
+  let (_, program_labeled) = sem_Program p 1
+  in program_labeled
 
 main :: IO ()
-main = undefined
+main =
+  do
+    hSetBuffering stdin LineBuffering
+
+    program_raw <- getLine >>= parseFile
+
+    let program_labeled = labeler program_raw
+
+    --  Debugging
+    print program_labeled
+
+    -- /Debugging
+
+    return ()
